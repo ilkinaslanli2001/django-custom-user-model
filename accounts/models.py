af337d071df52edce7d0ef950e597bcc4e34b997
ilkinaslanli2001 <ilkinaslanli0120@gmail.com>
2020-03-17 01:36:52 +0300

    Adding comments

--- accounts/models.py
+++ accounts/models.py
@@ -1,20 +1,25 @@
 from django.db import models
+
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
 
 
 class MyAccountManager(BaseUserManager):
+
     def create_user(self, username, email, password=None):
+
         if not email:
             return ValueError("Please enter you email")
         if not username:
             return ValueError("Please enter your username")
+
         user = self.model(
                           email=self.normalize_email(email),
                           username=username
                         )
+
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self.db) # Adding data to the database
         return user
 
     def create_superuser(self, username, email, password):
@@ -40,23 +45,33 @@ class Account(AbstractBaseUser):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=256)
     name = models.CharField(max_length=256)
+
+
+    # That's the default variables which we have to create
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
 
+    # What will be main parameter for sign in beside password (In our case it's username and password)
     USERNAME_FIELD = 'username'
+    # What others fields user has to enter
     REQUIRED_FIELDS = ['email', ]
+
     objects = MyAccountManager()
 
+    # Return username name
     def get_username(self):
         return self.username
 
+
+
     def has_perm(self, perm, obj=None):
-        return self.is_admin
+        return True
+
 
     def has_module_perms(self, app_label):
-        return True
+        return self.is_admin
 
 
 
--- custom_user_model/settings.py
+++ custom_user_model/settings.py
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# This variable tells app that we are using Account model as  User model
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
